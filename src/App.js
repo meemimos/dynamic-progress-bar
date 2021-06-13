820e799888a2c1080fffc672a01f8337bd1d6583
meemimos <sayabcz@hotmail.com>
2021-06-13 11:42:59 +1000

    UI Upgrade with button implementation

--- src/App.js
+++ src/App.js
@@ -57,21 +57,56 @@ function App() {
       });
   }
 
+  const handleButton = () => {
+    console.log('button clicked')
+  }
+
   return (
-    <div className="App">
-      {console.log(bars)}
-      <h1>Hello From Dynamic Progress Bar {bars.length}</h1>
-      {bars.map((bar) => {
-        return <ProgressBar key={bar.key}
-          className={
-            `mt-4 ${bar.active ? "progress-bar-active" : "progress-bar-deactive"}
+    <div className="custom-card">
+      {bars.length < 1 ?
+        <div className="text-center">
+          Loading..
+        </div>
+        :
+        <div className="page-header">
+          <h1>Progress Bar <small>Demo</small></h1>
+          {/* Progress Bar */}
+          <div>
+            {bars.map((bar) => {
+              return <ProgressBar key={bar.key}
+                className={
+                  `mt-4 ${bar.active ? "progress-bar-active" : "progress-bar-deactive"}
                    ${bar.percent > 100 ? "progress-bar-active-red" : ""}
                  `}
-          striped
-          variant={bar.percent > 100 ? "danger" : "primary"}
-          now={bar.percent < 0 ? 0 : bar.percent}
-          label={`${bar.percent < 0 ? 0 : bar.percent}%`} />
-      })}
+                striped
+                variant={bar.percent > 100 ? "danger" : "primary"}
+                now={bar.percent < 0 ? 0 : bar.percent}
+                label={`${bar.percent < 0 ? 0 : bar.percent}%`} />
+            })}
+          </div>
+          <div className="mt-4 text-center">
+            <h3>Controllers</h3>
+            <div className="btn-group btn-group-justified" role="group" aria-label="...">
+              <ul className="list-button">
+                {
+                  buttons.map(btn =>
+                    <li key={btn.key} style={{ marginLeft: "2px" }}>
+                      <div className="btn-group" role="group">
+                        <button className={`btn ${btn.value > 0 ? "btn-success" : "btn-danger"}`}
+                          onClick={handleButton}>{btn.value}
+                        </button>
+                      </div>
+                    </li>
+                  )
+                }
+              </ul>
+            </div>
+          </div>
+          <div className="text-center">
+            Progress bar limit: <small>{limit}</small>
+          </div>
+        </div>
+      }
     </div>
   );
 }
